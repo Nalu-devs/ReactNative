23d2e73250aded97f09955218261586e40f4c7ba
Nalu-devs <pordosol699@gmail.com>
2025-10-25 14:31:45 -0300

    .

---
+++ Hooks-React/components/UseStateHook.js
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Divider } from 'react-native-paper';
+
+export default function UseStateHook() {
+
+    const [numero, setNumero] = useState(0);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+
+      <Text style={styles.respostaText}>{valor}</Text>
+      <Divider style={styles.divider}/>
+      <TouchableOpacity style={[styles.btn, styles.btnAumenta]} onPress={() => { setValor(valor + 1); }}>
+        <Text style={styles.btnText}>Aumentar Valor</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.btn, styles.btnDiminui]} onPress={() => { setValor(valor - 1); }}>
+        <Text style={styles.btnText}>Diminuir Valor</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.btn, styles.btnMutilplica]} onPress={() => { setValor(valor * 2); }}>
+        <Text style={styles.btnText}>Mutiplicar Valor</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.btn, styles.btnDivide]} onPress={() => { setValor(valor / 2); }}>
+        <Text style={styles.btnText}>Dividir Valor</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 20
+  },
+  btn: {
+    backgroundColor: 'powderblue',
+    borderRadius: 8,
+    marginBottom: 30,
+    height: 40,
+    padding: 10,
+    width: 120,
+    alignItems: 'center'
+  },
+  btnAumenta: {
+    backgroundColor: 'green'
+  },
+  btnDiminui: {
+    backgroundColor: 'red'
+  },
+  btnMutilplica: {
+    backgroundColor: 'blue'
+  },
+  btnDivide: {
+    backgroundColor: 'gray'
+  },
+  btnText: {
+    color: '#fff'
+  },
+  respostaText: {
+    paddingBottom: 10,
+    fontSize: 40
+  },
+  divider: {
+    backgroundColor: '#000',
+    height: 2,
+    width: '80%',
+    marginBottom: 40
+  }
+});
